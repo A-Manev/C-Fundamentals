9e3a82e6f5dbc900df0ed1b3a691ee56350307a9
A-Manev <52919486+A-Manev@users.noreply.github.com>
2019-12-26 22:43:08 +0200

    Arrays - More Exercise

---
+++ Arrays - More Exercise/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfString = int.Parse(Console.ReadLine());
+            int stringSum = 0;
+            int[] result = new int[numberOfString];
+            for (int i = 0; i < numberOfString; i++)
+            {
+                stringSum = 0;
+                string name = Console.ReadLine();
+                for (int j = 0; j < name.Length; j++)
+                {
+                    int nameLenght = name.Length;
+                    int letter = name[j];
+
+                    switch (letter)
+                    {
+                        case 'a':
+                        case 'e':
+                        case 'i':
+                        case 'o':
+                        case 'u':
+                        case 'A':
+                        case 'E':
+                        case 'I':
+                        case 'O':
+                        case 'U':
+                            // vowel letter
+                            stringSum += nameLenght * letter;
+                            break;
+                        default:
+                            //consonant letter
+                            stringSum += letter / nameLenght;
+                            break;
+                    }
+                }
+                result[i] += stringSum;
+            }
+            Array.Sort(result);
+            Console.WriteLine(string.Join("\n", result));
+        }
+    }
+}
---
+++ Arrays - More Exercise/AllArrayProblemsAgain/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace AllArrayProblemsAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string input = Console.ReadLine();
+            int[] num = new int[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                num[i] = int.Parse(input[i].ToString());
+            }
+            Array.Sort(num);
+            Array.Reverse(num);
+            Console.WriteLine(string.Join("", num));
+        }
+    }
+}
